2abeff9bbce32395457cb7ae1123cae603d5ca52
Karl255 <49595438+Karl255@users.noreply.github.com>
2020-11-15 22:00:43 +0100

    General improvements - made the server return the requested file instead of index.html (duh) - added support for multiple MIME types - added code to decode %20 (spaces) in URLs

--- ArduinoStaticWebServer.ino
+++ ArduinoStaticWebServer.ino
@@ -4,19 +4,6 @@
 #include <sdios.h>
 #include <Regexp.h>
 
-// very basic success and failure response headers
-const char* http_response_200 =
-"HTTP/1.1 200 OK\r\n"
-"Content-Type: text/html\r\n"
-"Connection: close\r\n"
-"\r\n";
-
-const char* http_response_404 =
-"HTTP/1.1 404 Not Found\r\n"
-"Content-Type: text/html\r\n"
-"Connection: close\r\n"
-"\r\n";
-
 String requested_file; // global buffer for the requested file name
 byte mac[]{ 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // default MAC address from the Arduino Ethernet examples, should probably change this
 IPAddress ip(192, 168, 137, 177); // network ip address of the ethernet NIC
@@ -37,9 +24,9 @@ void loop() {
 
 	if (client) {
 		String get_request = "";
-		const char* selected_response;
+		String http_response_header;
 		File32 file;
-		
+
 		// read the first line of the HTTP header into get_request
 		// example of what it will read: GET / HTTP/1.1\r\n
 		while (client.connected() && client.available()) {
@@ -47,30 +34,34 @@ void loop() {
 			get_request += c;
 			if (c == '\n') break;
 		}
-		
+
 		// capture the requested file path
 		MatchState ms;
 		ms.Target(get_request.c_str());
 		ms.GlobalMatch("GET (.+) HTTP/", request_matched_callback);
-		
+
 		// default file is index.html
 		if (requested_file[requested_file.length() - 1] == '/')
 			requested_file += "index.html";
 		
+		// handle space URL encoding
+		requested_file.replace("%20", " ");
+
 		// check if the file exist, if yes, send a 200 and the file, otherwise send a 404
 		if (sd.exists(requested_file)) {
-			selected_response = http_response_200;
-			file = sd.open("/index.html");
+			http_response_header = build_http_response_header(true, requested_file);
+			file = sd.open(requested_file);
 		} else {
-			selected_response = http_response_404;
+			http_response_header = build_http_response_header(false, "/404.html");
 			file = sd.open("/404.html");
 		}
-		
-		// send the HTTP header and file to the client
-		client.print(selected_response);
+
+		// send the HTTP header and content to the client
+		client.print(http_response_header);
+		// this way of reading the file and sending it to the client is super slow and should be replaced with something faster
 		while (file.available())
 			client.write((char)file.read());
-		
+
 		file.close();
 		client.flush();
 		client.stop();
@@ -83,4 +74,39 @@ void request_matched_callback(const char* match, const unsigned int length, cons
 	char capture_buf[64];
 	ms.GetCapture(capture_buf, 0);
 	requested_file = String(capture_buf);
+}
+
+String build_http_response_header(bool success, String filename) {
+	String extension = filename.substring(filename.lastIndexOf('.') + 1);
+	String mime_type = "text/plain"; // default value is for plaintext
+	
+	//converting the file extension to the appropriate MIME type
+	// text
+	if (extension == "html" || extension == "htm") mime_type = "text/html";
+	else if (extension == "css")                   mime_type = "text/css";
+	else if (extension == "js")                    mime_type = "text/javascript";
+	else if (extension == "json")                  mime_type = "text/json";
+	// image
+	else if (extension == "png")                        mime_type = "image/png";
+	else if (extension == "jpg" || extension == "jpeg") mime_type = "image/jpeg";
+	else if (extension == "ico")                        mime_type = "image/vnd.microsoft.icon";
+	else if (extension == "svg")                        mime_type = "image/svg+xml";
+	else if (extension == "gif")                        mime_type = "image/gif";
+	// application
+	else if (extension == "rar") mime_type = "application/vnd.rar";
+	else if (extension == "tar") mime_type = "application/x-tar";
+	else if (extension == "zip") mime_type = "application/zip";
+	else if (extension == "7z")  mime_type = "application/x-7z-compressed";
+	else if (extension == "swf") mime_type = "application/x-shockwave-flash";
+	
+	// construct the final response header
+	return
+		String("HTTP/1.1 ")
+		+ (success ? "200 OK" : "404 Not Found")
+		+ "\r\n"
+		"Content-Type: "
+		+ mime_type
+		+ "\r\n"
+		"Connection: close\r\n"
+		"\r\n";
 }
--- README.md
+++ README.md
@@ -1,20 +1,23 @@
 # Arduino Static Web Server
-This is a very simple and basic (and shitty) static web server for Arduino using the Ethernet shield which also includes an SD card slot (in which the served files are stored).
+This is a very simple and basic (and shitty) static web server for Arduino using the Ethernet shield which also includes an SD card slot (in which the served files are stored). I barely use C++ so don't expect this code to be written in the best way possible.
 
 ### Supports:
 - GET requests
 - 404 error pages
 - default page (/ -> /index.html)
-- text/html content type
+- MIME types, a couple are implemented already and adding more is easy (unless the file contents affect the MIME type)
+- URL encoding code for spaces (only)
 
 ### Doesn't support:
-- POST and all other requests
+- POST and all other HTTP requests
 - all status codes except 200 and 404
 - any HTTP version other than 1.1
 - almost all HTTP header fields and those ones which are supported have very basic support
 - caching
 - redirection
 - multiple simultaneous connections
+- URL encoding/decoding (except spaces)
+
 
 ## Dependencies
 - built in SPI library
